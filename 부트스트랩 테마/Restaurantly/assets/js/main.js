645ff8b3873ba515d3aec2421c0b0787bb902cec
facio313 <faccio313@gmail.com>
2023-01-30 22:00:50 +0900

    ddit_CKS 20230130 산출물

--- 부트스트랩 테마/Restaurantly/assets/js/main.js
+++ 부트스트랩 테마/Restaurantly/assets/js/main.js
@@ -1,5 +1,5 @@
 /**
-* Template Name: Restaurantly - v3.10.0
+* Template Name: Restaurantly - v3.1.0
 * Template URL: https://bootstrapmade.com/restaurantly-restaurant-template/
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
